fe36a221a4250a43f5b8f2713801cb4109dd70da
chendureddy <130210899+chendureddy@users.noreply.github.com>
2023-10-12 15:13:36 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -29,9 +29,9 @@ linux
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+sudo yum upgrade -y
 # Add required dependencies for the jenkins package
 sudo yum install java-17-openjdk
-sudo yum install jenkins
+sudo yum install jenkins -y
 sudo systemctl daemon-reload
 cat /var/lib/jenkins/secrets/initialAdminPassword
